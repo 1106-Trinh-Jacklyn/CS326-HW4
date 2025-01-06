d60af5ca85606664ee4ad816f74e99ffc4ac0ff9
1106-Trinh-Jacklyn <144396202+1106-Trinh-Jacklyn@users.noreply.github.com>
2025-01-05 21:13:49 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//Name: Jacklyn Trinh
+//Date: 1/6/2025
+//Purpose: HW 4
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void loadGrid();
+void rotateGrid();
+
+int main(){
+
+
+  return 0;
+}
